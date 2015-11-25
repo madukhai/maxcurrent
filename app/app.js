82aa63eecbfb0cfee09fe4221f1895fc68f9a25d
madukhai <brainstation@BrainStations-MacBook-Air-3.local>
2015-11-24 21:37:42 -0500

    css

--- app/app.js
+++ app/app.js
@@ -1,17 +1,33 @@
-causeRepaintsOn = $("h1, h2, h3, p");
+causeRepaintsOn = $();
 $(document).ready(function(){
 	$('header .background img').css("width",window.innerWidth);
 	var height = $('header .background img').height();
+	
 	$('header').css("height",height);
+
+	var img_height = $('.clearfix div img').height();
+	$('.clearfix div').css({
+		
+		height: img_height
+	})
+
 
 });
 
 
 
 $(window).resize(function() {
-  causeRepaintsOn.css("z-index", 1);
-  $('header .background img').css("width",window.innerWidth);
-  var height = $('header .background img').height();
+  	causeRepaintsOn.css("z-index", 1);
+
+  	$('header .background img').css("width",window.innerWidth);
+  	var height = $('header .background img').height();
 	$('header').css("height",height);
+	$('.clearfix div img').css('width',window.innerWidth*0.5);
+	var img_height = $('.clearfix div img').height();
+	$('.clearfix div').css({
+		"width": "50%",
+		"height": img_height
+	})
+
 });
 
